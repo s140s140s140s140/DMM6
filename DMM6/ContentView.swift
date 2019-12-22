227b3be81d02b6653d6831f44c004a871dbea7e3
佐藤一成 <kazunarisato0309@gmail.com>
2019-12-22 19:52:58 +0900

    Initial Commit

---
+++ DMM6/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DMM6
+//
+//  Created by 佐藤一成 on 2019/12/22.
+//  Copyright © 2019 s140. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
