43eeef4ff204ea548ac7fb9edcb2548a04f951f2
佐藤一成 <kazunarisato0309@gmail.com>
2019-12-24 19:37:39 +0900

    12/24 これからenumに変換

---
+++ ActressListView.swift
@@ -0,0 +1,21 @@
+//
+//  ActressListView.swift
+//  DMM6
+//
+//  Created by 佐藤一成 on 2019/12/23.
+//  Copyright © 2019 s140. All rights reserved.
+//
+
+import SwiftUI
+
+struct ActressListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ActressListView_Previews: PreviewProvider {
+    static var previews: some View {
+        ActressListView()
+    }
+}
